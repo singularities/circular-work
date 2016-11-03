a7f7edd34e553b93100a38a876f68e61e60380c4
Antonio Tapiador del Dujo <atd@singularities.org>
2016-11-03 17:56:27 +0100

    User groups

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161103162936) do
+ActiveRecord::Schema.define(version: 20161103165139) do
+
+  create_table "groups", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "memberships", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "group_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["group_id"], name: "index_memberships_on_group_id"
+    t.index ["user_id"], name: "index_memberships_on_user_id"
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string   "title"

7688833105ba9d89e4c675781ca3699e2f5de32c
Antonio Tapiador del Dujo <atd@singularities.org>
2016-11-10 19:49:18 +0100

    rails api only app
    Notes:
    
    * Removed cookies, token is recommended as auth mechanism
    * Removed assets, they are served now from frontend

--- app/assets/javascripts/router.coffee
+++
@@ -1,5 +0,0 @@
-# For more information see: http://emberjs.com/guides/routing/
-
-CircularWorks.Router.map ()->
-  # @resource('posts')
-
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,64 +1,41 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :set_task, only: [:show, :update, :destroy]
 
   # GET /tasks
-  # GET /tasks.json
   def index
     @tasks = Task.all
+
+    render json: @tasks
   end
 
   # GET /tasks/1
-  # GET /tasks/1.json
   def show
-  end
-
-  # GET /tasks/new
-  def new
-    @task = Task.new
-  end
-
-  # GET /tasks/1/edit
-  def edit
+    render json: @task
   end
 
   # POST /tasks
-  # POST /tasks.json
   def create
     @task = Task.new(task_params)
 
-    respond_to do |format|
-      if @task.save
-        format.html { redirect_to @task, notice: 'Task was successfully created.' }
-        format.json { render :show, status: :created, location: @task }
-      else
-        format.html { render :new }
-        format.json { render json: @task.errors, status: :unprocessable_entity }
-      end
+    if @task.save
+      render json: @task, status: :created, location: @task
+    else
+      render json: @task.errors, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /tasks/1
-  # PATCH/PUT /tasks/1.json
   def update
-    respond_to do |format|
-      if @task.update(task_params)
-        format.html { redirect_to @task, notice: 'Task was successfully updated.' }
-        format.json { render :show, status: :ok, location: @task }
-      else
-        format.html { render :edit }
-        format.json { render json: @task.errors, status: :unprocessable_entity }
-      end
+    if @task.update(task_params)
+      render json: @task
+    else
+      render json: @task.errors, status: :unprocessable_entity
     end
   end
 
   # DELETE /tasks/1
-  # DELETE /tasks/1.json
   def destroy
     @task.destroy
-    respond_to do |format|
-      format.html { redirect_to tasks_url, notice: 'Task was successfully destroyed.' }
-      format.json { head :no_content }
-    end
   end
 
   private
@@ -67,7 +44,7 @@ class TasksController < ApplicationController
       @task = Task.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
+    # Only allow a trusted parameter "white list" through.
     def task_params
       params.require(:task).permit(:title, :description, :recurrence, :recurrence_match, :email, :notification_subject, :notification_body)
     end

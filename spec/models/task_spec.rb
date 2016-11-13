9000a9f0e5f0b34bc70bce8e9e1ebd0fd4ca4ebe
Antonio Tapiador del Dujo <atd@singularities.org>
2016-11-13 13:16:45 +0100

    Add more cases in model specs

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -3,19 +3,27 @@ require 'rails_helper'
 RSpec.describe Task, type: :model do
   fixtures :tasks
 
-  describe '.weekly' do
-    let(:weekly_task) { tasks(:weekly) }
+  let(:weekly_task)  { tasks(:weekly) }
+  let(:monthly_task) { tasks(:monthly) }
 
+  describe '.weekly' do
     it 'includes weekly tasks' do
       expect(Task.weekly).to include(weekly_task)
+    end
+
+    it 'does not include monthly tasks' do
+      expect(Task.weekly).not_to include(monthly_task)
     end
   end
 
   describe '.monthly' do
-    let(:monthly_task) { tasks(:monthly) }
-
     it 'includes monthly tasks' do
       expect(Task.monthly).to include(monthly_task)
     end
+
+    it 'does not include weekly tasks' do
+      expect(Task.monthly).not_to include(weekly_task)
+    end
+
   end
 end

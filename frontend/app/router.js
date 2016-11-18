572b098a8a4a0303acd6a2758a1941ad331e1a4b
David Tapiador <david.tapiador@envato.com>
2016-11-18 17:51:17 +0100

    add basic login with devise

--- frontend/app/router.js
+++ frontend/app/router.js
@@ -1,12 +1,14 @@
 import Ember from 'ember';
 import config from './config/environment';
+import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
 
-const Router = Ember.Router.extend({
+const Router = Ember.Router.extend(ApplicationRouteMixin).extend({
   location: config.locationType,
   rootURL: config.rootURL
 });
 
 Router.map(function() {
+  this.route('login');
   this.route('tasks', function() {
     this.route('index');
     this.route('new');

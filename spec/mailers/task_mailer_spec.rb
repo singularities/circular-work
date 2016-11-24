2abe6df02b8752a6ae58063c318b56dfc08b6e89
Antonio Tapiador del Dujo <atd@singularities.org>
2016-11-24 23:53:37 +0100

    Split task_mailer expectations in their own examples

--- spec/mailers/task_mailer_spec.rb
+++ spec/mailers/task_mailer_spec.rb
@@ -6,26 +6,40 @@ RSpec.describe TaskMailer, type: :mailer do
   describe "when task has turns" do
 
     let(:task)  { tasks(:weekly) }
+    let(:email) { TaskMailer.notification(task) }
 
-    it "should build the email" do
-      email = TaskMailer.notification(task)
-
+    it 'the email has the valid TO' do
       expect(email.to).to match_array(task.turns.first.emails)
+    end
+
+    it 'the email has the valid CC' do
       expect(email.cc).to include(task.email)
+    end
+
+    it 'the email has the valid subject' do
       expect(email.subject).to include(task.turns.first.groups.first.name)
-      expect(email.text_part.body.to_s).to include(task.turns.first.groups.first.name)
+    end
+
+    it 'the email has the valid text_part' do
+      expect(email.text_part.body.to_s)
+        .to include(task.turns.first.groups.first.name)
     end
   end
 
   describe "when task has no turns" do
 
     let(:task)  { tasks(:monthly) }
+    let(:email) { TaskMailer.notification(task) }
 
-    it "should build the email" do
-      email = TaskMailer.notification(task)
-
+    it 'the email has the valid TO' do
       expect(email.to).to include(task.email)
+    end
+
+    it 'the email has the valid subject' do
       expect(email.subject).to eq(task.notification_subject)
+    end
+
+    it 'the email has the valid text_part' do
       expect(email.text_part.body.to_s).to include(task.notification_body)
     end
   end

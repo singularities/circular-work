63cf572469d21078d66cacb378a488e688151f14
Antonio Tapiador del Dujo <atd@singularities.org>
2016-11-29 11:55:04 +0100

    Fix foreign key reference in migration

--- db/migrate/20161117092921_add_author_to_tasks.rb
+++ db/migrate/20161117092921_add_author_to_tasks.rb
@@ -1,5 +1,5 @@
 class AddAuthorToTasks < ActiveRecord::Migration[5.0]
   def change
-    add_reference :tasks, :author, foreign_key: true
+    add_reference :tasks, :author, foreign_key: { to_table: :users }
   end
 end

1d8a1b454666f0bca5df18bc813fdfe47dfc6141
Antonio Tapiador del Dujo <atd@singularities.org>
2016-12-12 14:34:05 +0100

    Fix task serializer

--- app/serializers/task_serializer.rb
+++ app/serializers/task_serializer.rb
@@ -1,5 +1,5 @@
 class TaskSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :recurrence, :recurrence_match, :email, :notification_subject, :notification_body
+  attributes :id, :title, :description, :recurrence, :recurrence_match, :notification_email, :notification_subject, :notification_body
 
   has_many :turns
 end

0aca99c4076581866c82e03c169271faee37767a
Antonio Tapiador del Dujo <atd@singularities.org>
2016-12-12 14:11:36 +0100

    Fix ember tests

--- frontend/tests/integration/components/task-notifications-test.js
+++ frontend/tests/integration/components/task-notifications-test.js
@@ -5,9 +5,13 @@ moduleForComponent('task-notifications', 'Integration | Component | task notific
   integration: true
 });
 
-test('it renders', function() {
+test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
   this.render(hbs`{{task-notifications}}`);
+
+  // Not sure if we have to waste time in integration tests
+  // I prefer unit and acceptance
+  assert.equal(this.$().text().indexOf("ToDo"), -1);
 });

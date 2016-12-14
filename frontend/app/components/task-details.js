0cc9109625acd60020e8156a5e2b4fe1b3019ce8
Antonio Tapiador del Dujo <atd@singularities.org>
2016-12-14 18:06:42 +0100

    Refactor cancel in task show controller

--- frontend/app/components/task-details.js
+++ frontend/app/components/task-details.js
@@ -6,21 +6,6 @@ export default Ember.Component.extend({
   showing: Ember.computed.not('editing'),
 
   actions: {
-    cancel() {
-
-      // Should use http://emberjs.com/api/data/classes/DS.Model.html#method_resetAttribute
-      // but it is not enabled by default
-      //
-      // var task = this.task;
-      //
-      // this.attributes.forEach(function(attr) {
-      //  task.resetAttribute(attr);
-      // });
-      this.task.rollbackAttributes();
-
-      this.set('editing', false);
-
-    },
     edit() {
       this.set('editing', true);
     },
--- frontend/app/components/task-notifications.js
+++ frontend/app/components/task-notifications.js
@@ -1,25 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  // There is probably a better way to get these
-  // attributes: [ 'notificationEmail', 'notificationSubject', 'notificationBody' ],
   showing: Ember.computed.not('editing'),
   actions: {
-    cancel() {
-
-      // Should use http://emberjs.com/api/data/classes/DS.Model.html#method_resetAttribute
-      // but it is not enabled by default
-      //
-      // var task = this.task;
-      //
-      // this.attributes.forEach(function(attr) {
-      //  task.resetAttribute(attr);
-      // });
-      this.task.rollbackAttributes();
-
-      this.set('editing', false);
-
-    },
     edit() {
       this.set('editing', true);
     },

5232d63cffbc3c503f0f9b6ed4effba7c8726119
Antonio Tapiador del Dujo <atd@singularities.org>
2016-12-15 15:48:28 +0100

    Turn groups selector

--- frontend/app/components/task-turns-row.js
+++ frontend/app/components/task-turns-row.js
@@ -5,9 +5,24 @@ export default Ember.Component.extend({
 
   showing: Ember.computed.not('editing'),
 
+  availableGroups: [],
+  selectedGroups: Ember.computed('turn.groups.@each', {
+    get() {
+      return this.get('turn.groups');
+    },
+    set(key, value) {
+      return this.get('turn').set('groups', value);
+    }
+  }),
+
   actions: {
     edit() {
       this.set('editing', true);
+    },
+    save() {
+      // TODO: save turn
+
+      this.set('editing', false);
     }
   }
 });
--- frontend/app/locales/en/translations.js
+++ frontend/app/locales/en/translations.js
@@ -108,6 +108,9 @@ export default {
       'row': {
         'edit': {
           'button': 'Edit'
+        },
+        'save': {
+          'button': 'Done!'
         }
       }
     }

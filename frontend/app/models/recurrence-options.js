60251b19ada2be2e96f81c51b227707b138f4988
Antonio Tapiador del Dujo <atd@singularities.org>
2016-12-16 11:06:35 +0100

    Revert "We don't support some recurrences by now"
    @nogates I expected to comment these options to don't show them to
    users, but it seems it is not so easy. The recurrence value sent is null
    
    This reverts commit 79221651db6dbfa31c4be70bfe71491b37cc00e0.

--- frontend/app/models/recurrence-options.js
+++ frontend/app/models/recurrence-options.js
@@ -1,8 +1,8 @@
 import Ember from 'ember';
 
 export default [
-//  Ember.Object.create({ value: 0, label: "Daily" }),
+  Ember.Object.create({ value: 0, label: "Daily" }),
   Ember.Object.create({ value: 1, label: "Weekly" }),
   Ember.Object.create({ value: 2, label: "Montly" }),
-//  Ember.Object.create({ value: 3, label: "Yearly" })
+  Ember.Object.create({ value: 3, label: "Yearly" })
 ];

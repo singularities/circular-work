49f6247c0373d89e6685a4535f0cbf4726db7f31
Antonio Tapiador del Dujo <atd@singularities.org>
2016-12-18 14:51:32 +0100

    Change name acording to domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,7 +45,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "circular-works_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "circular-work_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.

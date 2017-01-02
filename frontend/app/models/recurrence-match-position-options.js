da102eae522221cc8cf78f577164ac5d2b1c42d5
Antonio Tapiador del Dujo <atd@singularities.org>
2017-01-02 10:56:42 +0100

    Fix recurrence position values

--- frontend/app/models/recurrence-match-position-options.js
+++ frontend/app/models/recurrence-match-position-options.js
@@ -1,10 +1,10 @@
 import Ember from 'ember';
 
 const options = [
-  Ember.Object.create({ value: 0, label: "first" }),
-  Ember.Object.create({ value: 1, label: "second" }),
-  Ember.Object.create({ value: 2, label: "third" }),
-  Ember.Object.create({ value: 3, label: "fourth" }),
+  Ember.Object.create({ value: 1, label: "first" }),
+  Ember.Object.create({ value: 2, label: "second" }),
+  Ember.Object.create({ value: 3, label: "third" }),
+  Ember.Object.create({ value: 4, label: "fourth" }),
   Ember.Object.create({ value: -2, label: "nextToLast" }),
   Ember.Object.create({ value: -1, label: "last" })
 ];

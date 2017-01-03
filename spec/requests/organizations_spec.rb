da6b20ad575a8677781cda26e65321c8c4b8cf06
Antonio Tapiador del Dujo <atd@singularities.org>
2017-01-03 12:31:44 +0100

    Use @nogates shared examples for organization request spec

--- spec/requests/organizations_spec.rb
+++ spec/requests/organizations_spec.rb
@@ -2,9 +2,10 @@ require 'rails_helper'
 
 RSpec.describe "Organizations", type: :request do
   describe "GET /organizations" do
-    it "works! (now write some real specs)" do
-      get organizations_path
-      expect(response).to have_http_status(200)
-    end
+    let(:method) { :get }
+    let(:path) { organizations_path }
+    let(:data) { {} }
+
+    it_behaves_like 'is a valid request'
   end
 end
